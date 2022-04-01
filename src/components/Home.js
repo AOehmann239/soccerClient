8494496db32c9162cab6d0fec35f8c2cbee5bf6b
aoehmann239 <aoehmann239@gmail.com>
2022-04-01 09:11:25 -0400

    Added index on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
+import IndexTeams from './pets/IndexTeams';
+
 const Home = (props) => {
-	// const { msgAlert, user } = props
-	console.log('props in home', props)
+  // const { msgAlert, user } = props
+  const { user, msgAlert } = props;
 
-	return (
-		<>
-			<h2>Home Page</h2>
-		</>
-	)
-}
+  return (
+    <>
+      <h2>Home Page</h2>
+      <IndexTeams user={user} msgAlert={msgAlert} />
+    </>
+  );
+};
 
-export default Home
+export default Home;
